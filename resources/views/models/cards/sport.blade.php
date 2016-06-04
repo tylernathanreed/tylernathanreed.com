ab119cd2987ff39395bac8e7dafc9e004851d7ea
Tyler Reed <tylernathanreed@gmail.com>
2016-06-03 20:49:02 -0500

    + Added Content Header + Added Search Section

--- resources/views/models/cards/sport.blade.php
+++ resources/views/models/cards/sport.blade.php
@@ -3,7 +3,35 @@
 @section('content')
 
 	<div class="container">
-		<h1 class="--no-margin-top">{{ ucfirst($sport) }}</h1>
-	</div> <!-- </container> -->
+		<div class="content-header">
+			<h1 class="content-heading">{{ ucfirst($sport) }}</h1>
 
+			<div class="actions">
+				<a class="btn btn-primary action" href="{{ route('cards.create', $sport) }}">
+					<i class="glyphicon glyphicon-plus"></i>
+					<span>Create new {{ ucfirst($sport) }} Card</span>
+				</a>
+			</div> <!-- </actions> -->
+		</div> <!-- </content-header> -->
+
+		@include('models.cards.search')
+
+		<h2>Cards</h2>
+
+		@ifempty($cards)
+			<i>There are no Cards to display.</i>
+		@else
+			<ul class="list-group list-card">
+				@foreach($cards as $card)
+					<li class="list-group-item list-card-item">
+						@include('models.cards.teaser', compact('card'))
+					</li>
+				@endforeach
+			</li>
+		@endifempty
+
+		<div class="content-footer --center">
+			{!! $cards->appends($append)->render() !!}
+		</div> <!-- </content-footer> -->
+	</div> <!-- </container> -->
 @endsection
