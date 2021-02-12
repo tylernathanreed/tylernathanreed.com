1c7e54607ad7b20f33f6f838e93a5892beb0e9f2
Tyler Reed <tylernathanreed@gmail.com>
2021-02-12 06:48:32 -0600

    ~ Style fixes

--- resources/views/components/portfolio-modal.blade.php
+++ resources/views/components/portfolio-modal.blade.php
@@ -10,12 +10,12 @@
             </button>
             <div class="modal-body">
                 <div class="portfolio-modal-heading">
-                    <div class="portfolio-modal-icon mr-4{{ !empty($icon) ? " {$icon} text-white p-2" : '' }} bg-{{ $color }}">
+                    <div class="portfolio-modal-icon mr-2 mr-lg-4{{ !empty($icon) ? " {$icon} text-white p-md-2" : '' }} bg-{{ $color }}">
                         @if(is_null($icon))
                             <img class="img-fluid" src="{{ url("images/ecosystem/{$key}.min.svg") }}" alt="{{ $name }}" />
                         @endif
                     </div>
-                    <h2 class="portfolio-modal-title text-secondary mb-0" id="portfolio-modal-{{ $key }}-label">{{ $name }}</h2>
+                    <h2 class="portfolio-modal-title text-secondary mb-0 pr-4" id="portfolio-modal-{{ $key }}-label">{{ $name }}</h2>
                 </div>
 
                 <div class="leading-normal">{!! $slot !!}</div>
