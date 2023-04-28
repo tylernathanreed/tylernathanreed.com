cd00684f0708b960ae0e324728e7f245f00ab4df
Tyler Reed <tylernathanreed@gmail.com>
2023-04-28 16:09:46 -0500

    ~ Spacing

--- public/index.php
+++ public/index.php
@@ -3,10 +3,8 @@
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
- *
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
 define('LARAVEL_START', microtime(true));
 
 if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
